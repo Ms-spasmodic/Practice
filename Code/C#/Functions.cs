048a41c755fe4a63d763514b28ede7ecc593654a
Ms-spasmodic <ms.spasmodic@gmail.com>
2018-01-10 19:37:42 -0500

    Latest update

---
+++ Code/C#/Functions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Arg {0} : {1}", i, args[i]);
+            }
+
+            string[] myArgs = Environment.GetCommandLineArgs();
+            Console.WriteLine(string.Join(",  " , myArgs));
+
+            SayHello();
+
+            Console.ReadLine();
+        }
+        private static void SayHello()
+        {
+            string name = "";
+            
+            Console.Write("What is your name : ");
+            name = Console.ReadLine();
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
